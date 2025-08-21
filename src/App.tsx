54dce739bdc6eee09216ba71b85ae4c1ba7e56d6
realtravismartin <travis@realtravismartin.com>
2025-08-21 09:41:42 -0600

    Create missing index.html and React app files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Patient Advocacy Service</h1>
+        <p>
+          Welcome to the Patient Advocacy Service website - helping patients, providers, and employers navigate insurance approvals.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
